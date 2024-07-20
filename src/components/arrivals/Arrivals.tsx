b3fcb98fe94f23790617da84295202009481ff41
Nazarii <dzhaman.nazar2003@gmail.com>
2024-07-20 12:14:54 +0300

    Update Arrivals.tsx

--- src/components/arrivals/Arrivals.tsx
+++ src/components/arrivals/Arrivals.tsx
@@ -2,7 +2,19 @@ import { FC } from "react";
 import css from "./Arrivals.module.css";
 import { Card } from "../card/Card";
 
-export const Arrivals: FC = () => {
+interface ICard {
+    title: string,
+    description: string,
+    img: string
+}
+
+const arrivalsData: ICard[] = [
+    { title: "Hoodies & Sweatshirt", description: "Explore Now!", img: "path/to/image1.jpg" },
+    { title: "New Sneakers", description: "Check them out!", img: "path/to/image2.jpg" },
+    { title: "Stylish Jackets", description: "Get Yours Today!", img: "path/to/image3.jpg" }
+];
+
+export const Arrivals: FC<ICard> = () => {
 	return (
 		<section className={css.arrivals}>
 			<div className={css.arrivals_container}>
@@ -10,9 +22,9 @@ export const Arrivals: FC = () => {
 					<h2 className={css.arrivals_title}>New Arrivals</h2>
 				</div>
 				<div className={css.arrivals_cards}>
-					<Card />
-					<Card />
-					<Card />
+					{arrivalsData.map((card, index) => (
+						<Card key={index} title={card.title} description={card.description}/>
+					))}
 				</div>
 			</div>
 		</section>
